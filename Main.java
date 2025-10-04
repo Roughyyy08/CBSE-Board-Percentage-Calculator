94cd70c60f5562c9199db68262dd7bb903320cfd
Roughyyy08 <fraggod07@gmail.com>
2025-10-05 00:15:07 +0530

    Final commit

--- Main.java
+++ Main.java
@@ -2,25 +2,31 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-     System.out.println("Marks in Maths (out of 100)");
-     Scanner sc = new Scanner(System.in);
-     int m = sc.nextInt();
+        System.out.println("WELCOME TO CBSE BOARD PERCENTAGE CALCULATOR");
+        Scanner sc = new Scanner(System.in);
 
-     System.out.println("Marks in Physics (out of 100)");
-     int p = sc.nextInt();
+        System.out.println("Enter total marks of each subject (eg: 100, 80 ,etc.)");
+        int totalmarks = sc.nextInt();
 
-     System.out.println("Marks in Chemistry (out of 100)");
-     int c = sc.nextInt();
+        System.out.println("Marks in Maths");
+        int m = sc.nextInt();
 
-     System.out.println("Marks in English (out of 100)");
-     int e = sc.nextInt();
+        System.out.println("Marks in Physics");
+        int p = sc.nextInt();
 
-     System.out.println("Marks in Second Language (out of 100)");
-     int l = sc.nextInt();
+        System.out.println("Marks in Chemistry");
+        int c = sc.nextInt();
 
-     System.out.println((m+p+c+e+l)/500);
+        System.out.println("Marks in English");
+        int e = sc.nextInt();
 
-     sc.close();
+        System.out.println("Marks in Second Language");
+        int l = sc.nextInt();
+
+        float percentage = ((m+p+c+e+l)*100)/(totalmarks*5);
+
+        System.out.println("Your Percentage is : " + percentage);
+        sc.close();
 
     }
 }
